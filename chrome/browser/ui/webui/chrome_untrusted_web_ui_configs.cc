ea186a724c64a0f25b5d49d8d8e8faf4090ad250
Rıza Emre ARAS <r.emrearas@proton.me>
2026-09-01 12:58:11 +0300

    Remove: the two lens untrusted WebUI configs
    LensOverlayUntrustedUIConfig and LensSidePanelUntrustedUIConfig are
    removed from chrome_untrusted_web_ui_configs.cc with their includes.

--- chrome/browser/ui/webui/chrome_untrusted_web_ui_configs.cc
+++ chrome/browser/ui/webui/chrome_untrusted_web_ui_configs.cc
@@ -17,8 +17,6 @@
 #include "chrome/browser/ui/webui/compose/compose_untrusted_ui.h"
 #endif  // BUILDFLAG(ENABLE_COMPOSE)
 #include "chrome/browser/glic/selection/selection_overlay_untrusted_ui.h"
-#include "chrome/browser/ui/lens/lens_overlay_untrusted_ui.h"
-#include "chrome/browser/ui/lens/lens_side_panel_untrusted_ui.h"
 #include "chrome/browser/ui/webui/ai_overlay_dialog/ai_overlay_dialog_untrusted_ui.h"
 #endif  // defined(TOOLKIT_VIEWS)
 
@@ -53,10 +51,6 @@ void RegisterChromeUntrustedWebUIConfigs() {
         // BUILDFLAG(IS_ANDROID)
 
 #if defined(TOOLKIT_VIEWS)
-  map.AddUntrustedWebUIConfig(
-      std::make_unique<lens::LensOverlayUntrustedUIConfig>());
-  map.AddUntrustedWebUIConfig(
-      std::make_unique<lens::LensSidePanelUntrustedUIConfig>());
   map.AddUntrustedWebUIConfig(
       std::make_unique<ttc::AiOverlayDialogUntrustedUIConfig>());
   map.AddUntrustedWebUIConfig(
